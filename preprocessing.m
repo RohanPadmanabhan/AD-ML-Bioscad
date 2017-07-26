301e0d004122e8cc3e02eac29d5c13cb0b0a7a8c
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-07-26 21:07:08 +0100

    Apply some pre-processing functions to the data set

--- preprocessing.m
+++ preprocessing.m
@@ -1,9 +1,38 @@
 %%Preprocessing the data
 
-%% Clean slate
+% Clean slate
 clear;
 clc;
 
 %% Load the data
-filename = "AD-Non-Lesional.xlsx";
-rawData = readtable(filename);
+%filename = "AD-Non-Lesional.xlsx";
+%rawData = readtable(filename);
+load('rawADNonLesionalData.mat');
+
+%% Remove unneeded columns
+rawData.IL_12P40 = [];
+rawData.Status = [];
+rawData.lesional_Non_lesional = [];
+
+%% Replace Male and Female with numerics
+rawData.Gender = makeNumericGender(rawData.Gender, "M");
+
+%% Remove unknown values
+unknownLabels = ["not known", "unknown"];
+rawData.FLGCarrier = removeUnknownWords(rawData.FLGCarrier, unknownLabels);
+rawData.FLGNumberOfMutations = removeUnknownWords(rawData.FLGNumberOfMutations, unknownLabels);
+
+%% Remove outliers, log, and normalize columns with numeric data
+numericalDataStartColumn = 19;
+numericalDataEndColumn = 48;
+stdDevLimit = 5;
+
+for i = numericalDataStartColumn:numericalDataEndColumn
+    rawData(:,i) = array2table(removeNormalDistOutliers(table2array(rawData(:,i)), stdDevLimit));
+    rawData(:,i) = array2table(logAndNormalizeColumn(table2array(rawData(:,i))));
+end
+
+
+
+
+
