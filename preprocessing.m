556d4601a8d5ee80ca71d071689e720e86b51cd6
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-07-27 14:54:59 +0100

    Add code to remove underfilled columns to preprocessing script

--- preprocessing.m
+++ preprocessing.m
@@ -9,30 +9,57 @@ clc;
 %rawData = readtable(filename);
 load('rawADNonLesionalData.mat');
 
+
+%% Rename rawData
+preprocessedData = rawData;
+
 %% Remove unneeded columns
-rawData.IL_12P40 = [];
-rawData.Status = [];
-rawData.lesional_Non_lesional = [];
+preprocessedData.IL_12P40 = [];
+preprocessedData.Status = [];
+preprocessedData.lesional_Non_lesional = [];
+preprocessedData.FLGFailed_for = [];
+preprocessedData.DateOfBirth = [];
+preprocessedData.DateOfVisit = [];
 
 %% Replace Male and Female with numerics
-rawData.Gender = makeNumericGender(rawData.Gender, "M");
+preprocessedData.Gender = makeNumericGender(preprocessedData.Gender, "M");
 
 %% Remove unknown values
 unknownLabels = ["not known", "unknown"];
-rawData.FLGCarrier = removeUnknownWords(rawData.FLGCarrier, unknownLabels);
-rawData.FLGNumberOfMutations = removeUnknownWords(rawData.FLGNumberOfMutations, unknownLabels);
+preprocessedData.FLGCarrier = removeUnknownWords(preprocessedData.FLGCarrier, unknownLabels);
+preprocessedData.FLGNumberOfMutations = removeUnknownWords(preprocessedData.FLGNumberOfMutations, unknownLabels);
+
+clear unknownLabels
 
 %% Remove outliers, log, and normalize columns with numeric data
-numericalDataStartColumn = 19;
-numericalDataEndColumn = 48;
-stdDevLimit = 5;
+numericalDataStartColumn = 16;
+numericalDataEndColumn = 45;
+stdDevLimit = 1.96;
 
+% Remove normal distribution outliers and log normalize each column between start and end
 for i = numericalDataStartColumn:numericalDataEndColumn
-    rawData(:,i) = array2table(removeNormalDistOutliers(table2array(rawData(:,i)), stdDevLimit));
-    rawData(:,i) = array2table(logAndNormalizeColumn(table2array(rawData(:,i))));
+    preprocessedData(:,i) = array2table(removeNormalDistOutliers(table2array(preprocessedData(:,i)), stdDevLimit));
+    preprocessedData(:,i) = array2table(logAndNormalizeColumn(table2array(preprocessedData(:,i))));
 end
 
+clear i numericalDataStartColumn numericalDataEndColumn stdDevLimit
 
 
+%% Remove columns with lack of values
+dataStartColumn = 2;
+dataEndColumn = 45;
+minimumFillPercentage = 85;
+
+i = dataStartColumn; % First column to be tested
+while i <= dataEndColumn
+   if isUnderfilled(table2array(preprocessedData(:,i)), minimumFillPercentage)
+        preprocessedData(:,i) = [];
+        dataEndColumn = dataEndColumn - 1; %Now one fewer column
+   else
+       i = i + 1; % Move to next column
+   end
+end
+
+clear dataStartColumn dataEndColumn i minimumFillPercentage
 
 
