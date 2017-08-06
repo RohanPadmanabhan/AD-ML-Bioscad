2676357697343f5ddbc6ef9faff3cb5952d8f6a0
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-08-06 19:50:34 +0100

    Add code to use KNN to complete table.

--- preprocessing.m
+++ preprocessing.m
@@ -81,3 +81,45 @@ end
 
 
 clear n i dataStartColumn dataEndColumn minimumFillPercentage
+
+
+%% Fill in data using KNN
+
+% Save variables that should not be normalized
+subSCORAD = preprocessedData.SubjectiveSCORAD;
+objSCORAD = preprocessedData.ObjectiveSCORAD;
+ageDays = preprocessedData.AgeAtVisit_inDays_;
+skinType = preprocessedData.SkinType;
+
+% Temporarily normalize data
+preprocessedData.SubjectiveSCORAD = normalizeColumn(preprocessedData.SubjectiveSCORAD);
+preprocessedData.ObjectiveSCORAD = normalizeColumn(preprocessedData.ObjectiveSCORAD);
+preprocessedData.AgeAtVisit_inDays_ = normalizeColumn(preprocessedData.AgeAtVisit_inDays_);
+preprocessedData.SkinType_ = normalizeColumn(preprocessedData.SkinType);
+
+% Create a new table with only the numeric data
+[~, p] = size(preprocessedData);
+numOfStringCols = 1;
+numericsOnly = preprocessedData(:, numOfStringCols+1:p);
+
+% Use KNN on the numerics only table to fill in the blanks
+[nNumerics, pNumberics] = size(numericsOnly);
+for i=1:nNumerics
+    for j=1:pNumberics
+        if isnan(table2array(numericsOnly(i,j)))
+            preprocessedData{i, j + numOfStringCols} = fillBlank(numericsOnly, i, j);
+        end
+    end
+end
+
+
+% Replace normalized data
+preprocessedData.SubjectiveSCORAD = subSCORAD;
+preprocessedData.ObjectiveSCORAD = objSCORAD;
+preprocessedData.AgeAtVisit_inDays_ = ageDays;
+preprocessedData.SkinType = skinType;
+
+clear ageDays i j nNumerics numericsOnly numOfStringCols objSCORAD subSCORAD p pNumberics skinType
+
+
+
