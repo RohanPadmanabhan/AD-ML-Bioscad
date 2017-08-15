75b8ff6fd0b0a6cde2e0237c9884b8ee65e9150a
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-08-15 18:39:54 +0100

    Modify variable names for consistency.

--- Preprocessing/preprocessAll.m
+++ Preprocessing/preprocessAll.m
@@ -6,4 +6,4 @@ clc;
 
 [ppADLesional] = preprocessing('AD-lesional');
 [ppADNonLesional] = preprocessing('AD-non-lesional');
-[combinedNonLesional] = preprocessing('combined-non-lesional');
+[ppCombinedNonLesional] = preprocessing('combined-non-lesional');
