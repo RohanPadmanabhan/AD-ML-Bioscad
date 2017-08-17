7c63bdc2fae6a60b7ab7f97027f6d76be783bddb
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-08-17 11:57:14 +0100

    Complete elastic net for loop in parallel.

--- Elastic-net/elasticNet.m
+++ Elastic-net/elasticNet.m
@@ -37,16 +37,26 @@ clear testProportion trainCVPoints testPoints n
 
 %% Elastic net
 
+% Define constants
 nCross = 100;
 testProportion = 0.2;
 valProportion = 0.25;
+allowedSCORADDiff = 2;
 
-alpha = 0:0.1:1;
+% Define alpha and lambda ranges
+alpha = 0.1:0.1:1;
 lambda = 10.^(-3:0.5:5);
 
 
+% Pre-allocate space
+bestLambda = zeros(nCross, 1);
+bestAlpha = zeros(nCross, 1);
+predPerf = zeros(nCross, 1);
+predSucc = zeros(nCross, 1);
 
-for i = 1 : nCross
+tic
+
+parfor i = 1 : nCross
     
     % Split the data in to training and validation
     [xTest, xTrainVal, yTest, yTrainVal] = splitData(contData, objSCORAD, testProportion);
@@ -54,31 +64,40 @@ for i = 1 : nCross
     
     % Try every combination of lambda and alpha
     diffs = zeros(length(lambda), length(alpha));
-    for l = 1:length(lambda);
-        for a = 1:length(alpha);
+    for l = 1:length(lambda)
+        for a = 1:length(alpha)
             
             % Train the model on the training data
             [bl, fitInfo] = lasso(xTrain, yTrain, 'Lambda', lambda(l), 'Alpha', alpha(a));
-            bl = [fitInfo.Intercept; bl];
+            blFull = [fitInfo.Intercept; bl];
             
             % Validate the model on validation data
-            yPred = [ones(size(xVal, 1), 1), xVal] * bl;
+            yPred = [ones(size(xVal, 1), 1), xVal] * blFull;
             diffs(l,a) = rmse(yVal, yPred);
         end
     end
     
-    % Find the position of the values with minimum difference
-    [minRow, minCol] = find(diffs == max(diffs(:)));
+    % Find the position of the first value with minimum difference
+    % This prioritises low alpha values
+    [minDiff, minDiffLoc] = min(diffs(:));
+    [minDiffRow, minDiffCol] = ind2sub(size(diffs), minDiffLoc);
     
+    % Store the best alpha and lambda values
+    bestLambda(i) = lambda(minDiffRow);
+    bestAlpha(i) = alpha(minDiffCol);
     
+    % Re-train the model with the best alpha and lambda
+    [bl, fitInfo] = lasso(xTrain, yTrain, 'Lambda', bestLambda(i), 'Alpha', bestAlpha(i));
+    blFull = [fitInfo.Intercept; bl];
     
+    % Predict the values using the newly trained model
+    yPred = [ones(size(xVal, 1), 1), xVal] * blFull;
     
-    
-    
-    
-    
+    % Asses the performance
+    predPerf(i) = rmse(yVal, yPred);
+    predSucc(i) = proportionSuccessful(yTest, yPred, allowedSCORADDiff);
     
 end
 
 
-
+toc
