518c22e876aac6643ff599222c8783424b7540e1
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-08-22 22:12:53 +0100

    Add function to use general regression on two of the attributes.

---
+++ Elastic-net/generalRegression.m
@@ -0,0 +1,47 @@
+%% General regression on a small data subset
+
+%% Clean slate
+close all
+clear
+clc
+
+
+%% Load the preprocessed data
+%inpFilepath = input('Filepath for the data: ', 's');
+inpFilepath = '../preprocessed-combined-non-lesional.mat';
+load(inpFilepath);
+
+
+%% Select the data 
+
+% Select the output data
+%useObjSCORAD = input('Use objective SCORAD? (1 or 0) ');
+useObjSCORAD = 1;
+if useObjSCORAD
+    outVals = preprocessedData.ObjectiveSCORAD;
+else
+    outVals = preprocessedData.TotalSCORAD;
+end
+
+% Select the input data
+inpVals = [preprocessedData.IL_1a, preprocessedData.IL_1_];
+
+
+%% Split the data into testing and training
+testProportion = 0.2;
+[xTest, xTrain, yTest, yTrain] = splitData(inpVals, outVals, testProportion);
+
+%% Train the model
+model = fitglm(xTrain, yTrain);
+
+%% Predict results using model
+coeffs = model.Coefficients.Estimate;
+[n, ~] = size(yTest);
+yPred = [ones(n,1), xTest] * coeffs;
+
+%% Test the performace
+predPerf = rmse(yTest, yPred);
+mcid = 9;
+predSucc = proportionSuccessful(yTest, yPred, mcid);
+
+%% Draw a scatter graph of the test results
