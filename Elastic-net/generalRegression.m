b0b9327b850019f80fb2365957fe1d4136870292
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-08-22 22:42:38 +0100

    Test model on 100 splits of data set.

--- Elastic-net/generalRegression.m
+++ Elastic-net/generalRegression.m
@@ -11,8 +11,8 @@ clc
 inpFilepath = '../preprocessed-combined-non-lesional.mat';
 load(inpFilepath);
 
-
-%% Select the data 
+clear inpFilepath
+%% Select the data
 
 % Select the output data
 %useObjSCORAD = input('Use objective SCORAD? (1 or 0) ');
@@ -28,23 +28,44 @@ end
 % Select the input data
 inpVals = [preprocessedData.IL_1a, preprocessedData.IL_1_];
 
+clear useObjSCORAD
 
-%% Split the data into testing and training
+%% Train on multiple subsets
+
+% Assign constants
+[n, ~] = size(inpVals);
 testProportion = 0.2;
-[xTest, xTrain, yTest, yTrain] = splitData(inpVals, outVals, testProportion);
-
-%% Train the model
-model = fitglm(xTrain, yTrain);
-
-%% Predict results using model
-coeffs = model.Coefficients.Estimate;
-[n, ~] = size(yTest);
-yPred = [ones(n,1), xTest] * coeffs;
-
-%% Test the performace
-predPerf = rmse(yTest, yPred);
+nCross = 100;
 mcid = 9;
-predSucc = proportionSuccessful(yTest, yPred, mcid);
 
-%% Draw a scatter graph of the test results
-drawScatterPredictions(scoradType, yTest, yPred);
+% Pre-allocate space for arrays used in loop
+predPerf = zeros(nCross, 1);
+predSucc = zeros(nCross, 1);
+
+% Pre-allocate space for results
+numTestCases = n * testProportion;
+yPredFull = zeros(nCross, numTestCases);
+yTestFull = zeros(nCross, numTestCases);
+
+parfor i = 1:nCross
+    % Split the data
+    [xTest, xTrain, yTest, yTrain] = splitData(inpVals, outVals, testProportion);
+    
+    % Train the model
+    model = fitglm(xTrain, yTrain);
+    
+    % Predict results using model
+    coeffs = model.Coefficients.Estimate;
+    [n, ~] = size(yTest);
+    yPred = [ones(n,1), xTest] * coeffs;
+    
+    % Save the results
+    yPredFull(i, :) = yPred;
+    yTestFull(i, :) = yTest;
+    
+    % Test the performace
+    predPerf(i) = rmse(yTest, yPred);
+    predSucc(i) = proportionSuccessful(yTest, yPred, mcid);
+end
+
+clear n nCross numTestCases testProportion
