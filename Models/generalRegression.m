b588ebbe6db09f120ff3705203d38a225af779e9
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-08-25 12:08:29 +0100

    Remove high and low values from general regression.

--- Models/generalRegression.m
+++ Models/generalRegression.m
@@ -50,6 +50,10 @@ parfor i = 1:nCross
     [n, ~] = size(yTest);
     yPred = [ones(n,1), xTest] * coeffs;
     
+    % Remove high and low values from prediction
+    yPred = yPred .* (yPred > 0);
+    yPred = replaceHighValues(yPred, maxSCORAD);
+    s
     % Save the results
     yPredFull(i, :) = yPred;
     yTestFull(i, :) = yTest;
