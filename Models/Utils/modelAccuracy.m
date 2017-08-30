5e93d31750c417be44dcfa082cbdcad7f93ea62c
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-08-30 12:12:15 +0100

    Add comments to modelAccuracy.

--- Models/Utils/modelAccuracy.m
+++ Models/Utils/modelAccuracy.m
@@ -1,5 +1,8 @@
 function [acc] = modelAccuracy(pred, test, mcid)
 
+% Finds the accuracy of each cross validation iteration
+% Each iteration is a single row in the input matrices
+
 [~, predsPerCross] = size(test);
 diffsMean = pred - test;
 diffsMean = (diffsMean < mcid) & (diffsMean > -mcid);
