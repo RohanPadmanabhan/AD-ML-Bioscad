bc12bb37d690c208b2180945335011a889d84697
RohanE5450DualBoot <rohan@rohan-Latitude-E5450.com>
2017-08-30 11:21:25 +0100

    Update testing to set values within mcid to 0.

--- Models/Utils/rankSumTesting.m
+++ Models/Utils/rankSumTesting.m
@@ -7,9 +7,17 @@ clc
 
 %% Load the data and remove unnecesary variables
 
-filePath = input(' Enter the results filename: ', 's');
+% Load the data file
+filePath = '../Results/oSCORAD-logged.mat';
+%filePath = input(' Enter the results filename: ', 's');
 load(filePath);
 clearvars -except yTestFull yPredFull
+
+% Get mcid
+mcid = 9;
+%[~, mcid, ~, ~] = extractOutputs();
+
+
 
 clear filePath
 
@@ -41,7 +49,18 @@ meanVals = reshape(meanVals, [], 1);
 clear predsPerCross
 
 %% Calculate the t-tests interval
-[p, h] = ranksum(yPredFull, meanVals);
+
+% Calculate the difference between the predictions and average
+diffs = yPredFull - meanVals;
+
+% Replace values within mcid with 0
+diffs = diffs .* (diffs > mcid | diffs < -mcid);
+
+% Carry out the t-test
+[h, p] = ttest(diffs);
+
+
+%% Display the results
 
 if (h)
     disp(['The values are different from the average. Statistical significance of: ', num2str(p)]);
