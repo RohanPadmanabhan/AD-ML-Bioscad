373dd8c4107b0150e51511e214d103030767ffcf
rp1615 <rp1615@ic.ac.uk>
2017-09-05 16:05:53 +0100

    Create function to return struct with same fields as required for LR o individual attributes

---
+++ Models/Utils/createIndivAttrStruct.m
@@ -0,0 +1,14 @@
+function [exampleStruct] = createIndivAttrStruct()
+
+% Create a struct with the same fields as the struct used for individual
+% attribute logistic regression
+
+exampleStruct = struct();
+exampleStruct.threshold = 0;
+exampleStruct.accuracy = 0;
+exampleStruct.evaluation = struct();
+exampleStruct.coefficients = 0;
+exampleStruct.yPredFull = 0;
+exampleStruct.yTestFull = 0;
+exampleStruct.varNames = [];
+exampleStruct.attribute = '';
