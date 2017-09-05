fd4d9d2e996a2bab4349b1290870a443cfa5b84f
rp1615 <rp1615@ic.ac.uk>
2017-09-05 16:06:49 +0100

    Modify funtion to preallocate space for tructs.

---
+++ Models/Classification-Models/indivAttrLR.m
@@ -0,0 +1,57 @@
+%% Perform logistical regression using the individual attributes
+
+%% Clean slate
+clear
+clc
+close all
+
+addpath('../Utils/');
+
+%% Load the data and get the file names
+
+%inputFilename = input('Enter the input file path: ', 's');
+inputFilename = '../../preprocessed-combined-non-lesional.mat';
+load(inputFilename);
+
+%outputFileName = input('Enter the output file path: ', 's');
+
+clear fullFile prefix extension rawData inputFilename
+
+%% Extract the output data
+outData = preprocessedData.ObjectiveSCORAD;
+outData = double(outData > 0);
+
+%% Define constants and pre-allocate space
+
+% Define start and end values for continuous data
+contDataStart = 19;
+[~, contDataEnd] = size(preprocessedData);
+numAttr = contDataEnd - contDataStart + 1;
+
+% Preallocate space for structs
+singleResults = repmat(createIndivAttrStruct(), numAttr, 1 );
+
+%% Perform logistic regression
+
+for i = contDataStart : contDataEnd
+    
+    % Define the singular attribute for the input data
+    inpData = table2array(preprocessedData(:, i));
+    
+    % Define the attribute names
+    attributeName = preprocessedData.Properties.VariableNames(i);
+    varNames = ['Constant', attributeName];
+    
+    % Perform logistic regression
+    tempResults = logisticRegression(inpData, outData);
+    
+    % Add the variable names
+    tempResults.varNames = varNames;
+    tempResults.attribute = attributeName;
+    
+    % Assign the results in to the array
+    index = i - contDataStart + 1;
+    singleResults(index) = tempResults;
+end
+
+clearvars -except preprocessedData singleResults
