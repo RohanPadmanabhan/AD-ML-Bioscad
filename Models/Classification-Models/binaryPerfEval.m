73a3b0b6f83ead5b168fcdee0ea65044e657e38a
rp1615 <rp1615@ic.ac.uk>
2017-09-05 12:45:37 +0100

    Add funtion to evaluate binary performance.

---
+++ Models/Classification-Models/binaryPerfEval.m
@@ -0,0 +1,54 @@
+function [eval] = binaryPerfEval(pred, actual)
+% Input 1: Array of predicted binary classification data
+% Input 2: Array of actual binary clasification data
+
+
+%% Find the locations of the positive values
+posLocs = (actual==1);
+
+%% Find the actual positive and negative values
+numTotal = length(actual);
+numPositives = sum(actual);
+numNegatives = numTotal - numPositives;
+
+%% Find the confusion matrix values
+truePos = sum(actual(posLocs)==pred(posLocs));
+trueNeg = sum(actual(~posLocs)==pred(~posLocs));
+falsePos = numNegatives-trueNeg;
+falseNeg = numPositives-truePos;
+
+%% Compute measures of accuracy
+accuracy = (truePos+trueNeg)/numTotal;
+sensitivity = truePos/numPositives; % TP rate
+specificity = trueNeg/numNegatives; % TN rate
+precision = truePos/(truePos+falsePos);
+
+%% Compute the F-Score (avoiding divide by zero errors)
+if truePos>0
+    fScore = 2*((precision*sensitivity)/(precision + sensitivity));
+else
+    fScore = 0;
+end
+
+%% Compute the Matthews correlation coefficient (avoiding divide by zero errors)
+if (truePos + falsePosv == 0)||(trueNeg + falseNeg  == 0)
+    mcc=0;
+else
+    numerator = truePos*trueNeg-falsePos*falseNeg;
+    denominator = sqrt((truePos+falsePos)*(truePos+falseNeg)*(trueNeg+falsePos)*(trueNeg+falseNeg));
+    mcc = numerator / denominator;
+end
+
+
+%% Compute the diagnostic odds ratio
+dor = (truePos*trueNeg) / (falsePos*falseNeg);
+
+%% Bundle the results in to a struct
+eval = struct();
+eval.accuracy = accuracy;
+eval.sensitivity = sensitivity;
+eval.specificity = specificity;
+eval.precision = precision;
+eval.fScore = fScore;
+eval.mcc = mcc;
+eval.dor = dor;
