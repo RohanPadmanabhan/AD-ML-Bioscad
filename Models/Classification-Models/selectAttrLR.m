dfaae4bd5577347479c422c8ca0a3612fe8842c7
RohanPadmanabhan <rohan@rohan-Latitude-E5450.com>
2017-09-06 13:30:14 +0100

    Add function to perform LR on user selected attributes.

---
+++ Models/Classification-Models/selectAttrLR.m
@@ -0,0 +1,34 @@
+%% Perform logistical regression using the full data set
+
+%% Clean slate
+clear
+clc
+close all
+
+addpath('../Utils/');
+
+%% Load the data and get the file names
+
+%inputFilename = input('Enter the input file path: ', 's');
+inputFilename = '../../preprocessed-combined-non-lesional.mat';
+load(inputFilename);
+
+%outputFileName = input('Enter the output file path: ', 's');
+
+clear fullFile prefix extension rawData inputFilename
+
+%% Extract the output data
+outData = preprocessedData.ObjectiveSCORAD;
+outData = double(outData > 0);
+
+%% Extract the input data
+[inpDataFull, varNames] = extractCombinedInpData(preprocessedData);
+
+%% Select the variables to use
+inpData = selectAttributes(inpDataFull, varNames);
+
+%% Perform logistic regression
+fullDataRes = logisticRegression(inpData, outData);
+fullDataRes.varNames = varNames;
+
+clearvars -except fullDataRes
