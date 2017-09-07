b810e24218acf537e55d9f627bc0618cdbfb5466
RohanPadmanabhan <rohan@rohan-Latitude-E5450.com>
2017-09-07 11:39:20 +0100

    Write function to perform LR on the sorted n top performing attributes.

---
+++ Models/Classification-Models/topAttrLR.m
@@ -0,0 +1,48 @@
+%% Use logistic regression on the top attributes
+
+%% Clean slate
+clear
+clc
+
+%% Load the data
+load('Results/indivAttrLR.mat');
+
+%% Sort the input data so best attributes are first
+bestAttrOrder = singleResults.attributeNumber;
+inpDataFull = inpDataFull(:, bestAttrOrder);
+
+%% Pre-allocate space
+[~, numAttr] = size(inpDataFull);
+nBestResults = repmat(createNTopAttrStruct(), numAttr, 1 );
+
+%% Perform logistic regression
+
+for i = 1 : numAttr
+    
+    % Turn off warnings
+    warning('off', 'all');
+    
+    % Define the attributes to use for the input data
+    inpData = inpDataFull(:, 1:i);
+    
+    % Perform logistic regression
+    tempResults = logisticRegression(inpData, outData);
+    
+    % Add the number of attributes used
+    tempResults.numTopAttributes = i;
+    
+    % Assign the results in to the array
+    nBestResults(i) = tempResults;
+end
+
+clear i tempResults numAttr singularInpData tempResults attributeName
+
+%% Sort the results by in to best values
+
+% Convert the results to a table and sort by accuracy
+accuracyCol = 2;
+nBestResults = struct2table(nBestResults);
+nBestResults = sortrows(nBestResults, accuracyCol, 'Descend');
+
+% Reorder the attributes
+nBestResults = [nBestResults(:,7), nBestResults(:,1:6)];
